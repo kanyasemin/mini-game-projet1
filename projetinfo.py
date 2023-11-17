70fc1ec995f52fd345180cb63e387b36108f8dfb
poporigolo <150429931+poporigolo@users.noreply.github.com>
2023-11-17 09:53:06 +0100

    Update projetinfo.py

--- projetinfo.py
+++ projetinfo.py
@@ -39,6 +39,7 @@ def update_p(letter,p,m):
     else: update_p(input("Quel déplacement (z,q,s ou d stp)?"),p,m)
     return p
 
+#rajouter test dans update pour s'arrêter devant le mur (même principe que les limites)
 
 
 print(display_map_and_char(map,dico,(update_p(d,perso))))
